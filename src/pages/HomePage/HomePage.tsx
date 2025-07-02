451b462953eb64f57cbab8dc32bcd28a420cf61e
Asmaa Aman <asmaa.aman91@gmail.com>
2025-07-02 20:16:27 +0300

    add logout

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,26 @@
 import { Button } from "@mui/material";
+import CookieServices from "../../services/CookieServices/CookieServices";
+
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const name = CookieServices.get("token");
+  console.log("first ", name);
+  const navigate = useNavigate();
+  const handleLogOut = () => {
+    CookieServices.remove("token");
+    navigate("/login");
+  };
   return (
     <>
       <div>HomePage</div>
-      <Button type="submit" className="submit-btn" fullWidth variant="outlined">
+      <Button
+        type="submit"
+        className="submit-btn"
+        fullWidth
+        variant="outlined"
+        onClick={() => handleLogOut()}
+      >
         Log out
       </Button>
     </>
