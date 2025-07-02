242559a4fb07256f05dfd88b21837844adf860c6
nadagmal <nadagamalsobhy@gmail.com>
2025-07-02 18:05:38 +0300

    starting work in reset pass

--- src/pages/ResetPassword/ResetPassword.tsx
+++ src/pages/ResetPassword/ResetPassword.tsx
@@ -1,10 +1,34 @@
+import { axiosInstance, USERS_URLS } from "@/services/EndPoints/EndPoints";
+import axios from "axios";
 import React from "react";
+import { useForm } from "react-hook-form";
+
+interface IResetPassword
+{
+  otp:string , 
+  password:string , 
+  confirmPassword: string;
+}
+
+
+const {register , formState :{errors} , handleSubmit} = useForm<IResetPassword>({mode:'onChange'});
+
+const onSubmit =async (data)=>
+{
+  try{
+      const response = await axiosInstance.post(USERS_URLS.RESET_PASSWORD , data)
+  }
+  catch (error)
+  {
+      console.log(error)
+  }
+}
 
 const ResetPassword = () => {
   return (
-    <div>
-      <h2>ResetPassword</h2>
-    </div>
+    <>
+     
+    </>
   );
 };
 
