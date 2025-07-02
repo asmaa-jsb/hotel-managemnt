194a9b90a3ca9dae0acce05ceaa663a7cd916a53
Asmaa Aman <asmaa.aman91@gmail.com>
2025-07-02 17:59:30 +0300

    add logout

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,14 @@
+import { Button } from "@mui/material";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <div>HomePage</div>
+      <Button type="submit" className="submit-btn" fullWidth variant="outlined">
+        Log out
+      </Button>
+    </>
+  );
 };
 
 export default HomePage;
