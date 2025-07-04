fa877b5f1705bb7c17010d01eef8a013d67d05b1
shimaa2468 <shamazourob@gmail.com>
2025-07-04 05:50:15 -0700

    use react-query mutation for user registration

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/global.css";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const Queryclient=new QueryClient()
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={Queryclient}>
+          <App />
+    </QueryClientProvider>
+ 
   </StrictMode>
 );
