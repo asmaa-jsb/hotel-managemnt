aa31fde0869b459465effacc62274672edb314aa
nadagmal <nadagamalsobhy@gmail.com>
2025-07-15 14:29:38 +0300

    change the profile path in index to export {default as Profile } from ../pages/AdminPortal/Profile/Profile

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,6 @@
 /****************shared pages*********************** */
 export { default as NotFound } from "./NotFound/NotFound";
-export {default as Profile } from "./Profile/Profile";
+export {default as Profile } from "../pages/AdminPortal/Profile/Profile";
 /*************Authentication*******************/
 export { default as Login } from "./Authentication/Login/Login";
 export { default as Register } from "./Authentication/Register/Register";
