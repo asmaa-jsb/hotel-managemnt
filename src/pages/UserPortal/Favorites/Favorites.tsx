35da36d764d5f4c41edf353e20788376caa7a261
shimaa2468 <shamazourob@gmail.com>
2025-07-19 05:35:58 -0700

    Faviourete component

---
+++ src/pages/UserPortal/Favorites/Favorites.tsx
@@ -0,0 +1,16 @@
+import UserHeader from "@/components/UserSharedModual/UserHeader/UserHeader";
+import React from "react";
+
+const Favorites = () => {
+  return (
+    <>
+      <UserHeader
+        title={"300"}
+        description="Bogor, Indonesia"
+        page="Your Favorites"
+      />
+    </>
+  );
+};
+
+export default Favorites;
--- src/services/EndPoints/EndPoints.ts
+++ src/services/EndPoints/EndPoints.ts
@@ -110,3 +110,4 @@ export const COMMENTS_URLS ={
 
   
 }
+
