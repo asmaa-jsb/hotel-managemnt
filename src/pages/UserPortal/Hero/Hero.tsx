0723017220879989b5ad1342d62d30e950aed685
nadagmal <nadagamalsobhy@gmail.com>
2025-07-20 20:56:44 +0300

    making explore button in landing page

--- src/pages/UserPortal/Hero/Hero.tsx
+++ src/pages/UserPortal/Hero/Hero.tsx
@@ -36,7 +36,7 @@ const Hero = () => {
   const handleExplore = () => {
     const from = format(dateRange[0].startDate, "yyyy-MM-dd");
     const to = format(dateRange[0].endDate, "yyyy-MM-dd");
-    navigate(`/room-explore/search?from=${from}&to=${to}&capacity=${capacity}`);
+    navigate(`/explore?from=${from}&to=${to}&capacity=${capacity}`);
   };
 
   return (
@@ -227,28 +227,17 @@ const Hero = () => {
                 </IconButton>
               </Box>
 
-              <IconButton
-                onClick={() => setCapacity((prev) => prev + 1)}
-                sx={{
-                  height: "100%",
-                  width: "48px",
-                  borderRadius: 0,
-                  backgroundColor: "#32C99F",
-                  color: "#fff",
-                  "&:hover": { backgroundColor: "#2cb98e" },
-                }}
-              >
-                <AddIcon />
-              </IconButton>
+             
             </Box>
 
             <Box pt={{ md: 4, xs: 2 }}>
               <ReusableButton
-                label="Explore"
-                type="submit"
-                padding="8px 85px"
-              />
-            </Box>
+                  label="Explore"
+                  type="button"
+                  padding="8px 85px"
+                  onClick={handleExplore}
+                />
+        </Box>
           </Box>
 
           {/* Right Image Section */}
