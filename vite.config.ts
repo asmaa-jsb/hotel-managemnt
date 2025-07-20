ca5786738d9bd41c6265a21a5c2996ccad2db41b
shimaa2468 <shamazourob@gmail.com>
2025-07-20 07:51:12 -0700

    fix: update deploy to run on development branch

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: '/hotel-managemnt/',
+  base: "/hotel-managemnt/",
   resolve: {
     alias: {
       "@": "/src",
